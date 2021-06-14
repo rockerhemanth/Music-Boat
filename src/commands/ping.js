186b8dc42707845651e9164877b95d6104291dca
M s v <78690237+Just-Msv@users.noreply.github.com>
2021-06-14 11:42:27 +0530

    Create ping.js

---
+++ src/commands/ping.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "ping",
+    aliases: ["pi"],
+    exec: (msg,text) => {
+        const latency = Date.now() - msg.createdTimestamp;
+        msg.channel.send(`🏓 Pong! \`${latency}ms\`. API Latency is \`${Math.round(msg.client.ws.ping)}ms\``);
+    }
+};
+© 2021 GitHub, Inc.
