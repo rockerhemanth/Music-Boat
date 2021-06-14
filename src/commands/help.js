5b9377b13b998099944adb422da153562d72c4d9
M s v <78690237+Just-Msv@users.noreply.github.com>
2021-06-14 11:37:27 +0530

    Create help.js

---
+++ src/commands/help.js
@@ -0,0 +1,28 @@
+const util = require("../util");
+
+const unlisted = ["eval", "source"];
+prefix=process.env.PREFIX;
+module.exports = {
+    name: "help",
+    aliases: ["h", "hp"],
+    exec: (msg) => {
+        const commands = msg.client.commands
+            .filter(c => !unlisted.includes(c.name))
+            .map(c => `\`${c.name}\``);
+
+        const embed = util.embed()
+            .setAuthor("| Aqua Help Menu: all", msg.client.user.displayAvatarURL())
+            .addField(" **❯ INFO**", `My Prefix is\`${prefix}\` or Just @Mention Me :fingers_crossed:`)
+            .addField(" **❯ MUSIC[14] :**", " `play`, `loop`, `lyrics`, `nowplaying`, `pause`, `queue`, `remove`, `resume`, `search`, `shuffle`, `skip`, `skipto`, `stop`, `volume`")
+            .addField(" **❯ FILTERS & MISC[5] :**","`24*7`,`bassboost`, `autoplay`, `seek`,`move`")
+            .addField(" **❯ UTILITIES[4] :**","`stats`,`invite`,`help`, `ping`")
+            .setFooter(`© ${msg.guild.me.displayName}`)
+            .setColor("f8a408")
+            .setThumbnail(msg.client.user.displayAvatarURL({ dynamic: true, size: 2048 }))
+            .setTimestamp();
+
+        msg.channel.send(embed);
+    }
+};
+
+console.log("help working");
