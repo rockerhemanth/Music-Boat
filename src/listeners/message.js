74ba817efee4ee3989d61d4b27e040c4f675d80c
M s v <78690237+Just-Msv@users.noreply.github.com>
2021-08-08 20:40:16 +0530

    Update message.js

--- src/listeners/message.js
+++ src/listeners/message.js
@@ -1,3 +1,4 @@
+
 const util = require("../util");
 
 module.exports = {
@@ -5,25 +6,16 @@ module.exports = {
     exec: async (client, msg) => {
         if (!msg.guild) return;
         if (msg.author.bot) return;
-        if (msg.channel.type === "dm") return;
-        
-        let prefix = db.get(`prefix_${msg.guild.id}`);
-        if (prefix === null) prefix = process.env.PREFIX;
-        
-        if(msg.content === `<@${client.user.id}>` || msg.content === `<@!${client.user.id}>`){
-            msg.channel.send(util.embed().setDescription(`My prefix here is \`${prefix}\` . You can play music by joining a voice channel and typing \`${prefix}play\`. The command accepts song names, video links, and playlist links.`).setColor("BLACK"));   
-        }
-        const escapeRegex = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-        const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
 
-        if(!prefixRegex.test(msg.content)) return;
-        const [, matchedPrefix] = msg.content.match(prefixRegex);
-        const args = msg.content.slice(matchedPrefix.length).trim().split(/ +/g);
+        const prefix = msg.content.toLowerCase().startsWith(client.prefix) ? client.prefix : `<@!${client.user.id}>`;
+        if (!msg.content.toLowerCase().startsWith(prefix)) return;
+        
+        const args = msg.content.slice(prefix.length).trim().split(/ +/g);
         const commandName = args.shift().toLowerCase();
         const command = client.commands.get(commandName) || client.commands.find(c => c.aliases && c.aliases.includes(commandName));
         if (command) {
             try {
-                await command.exec(msg,args);
+                await command.exec(msg, args);
             } catch (e) {
                 console.error(e);
             }
