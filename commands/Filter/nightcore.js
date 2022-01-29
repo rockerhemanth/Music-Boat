bca534a4ae4933008ac8fc3c2205e513843e2479
M S V <78690237+Just-Msv@users.noreply.github.com>
2022-01-29 15:08:20 +0530

    Fully Updated Version

---
+++ commands/Filter/nightcore.js
@@ -0,0 +1,50 @@
+const {
+  MessageEmbed
+} = require(`discord.js`);
+const config = require(`../../botconfig/config.json`);
+const ee = require(`../../botconfig/embed.json`);
+const emoji = require(`../../botconfig/emojis.json`);
+const DBL = require('@top-gg/sdk');
+module.exports = {
+  name: `nightcore`,
+  category: `Filter`,
+  aliases: ["nc"],
+  description: `Applies a Nightcore Filter`,
+  usage: `nightcore`,
+  parameters: {"type":"music", "activeplayer": true, "previoussong": false},
+  run: async (client, message, args, guildData, player, prefix) => {
+
+    try {
+      player.node.send({
+        op: "filters",
+        guildId: message.guild.id,
+        equalizer: player.bands.map((gain, index) => {
+            var Obj = {
+              "band": 0,
+              "gain": 0,
+            };
+            Obj.band = Number(index);
+            Obj.gain = Number(gain)
+            return Obj;
+          }),
+        timescale: {
+              "speed": 1.165,
+              "pitch": 1.125,
+              "rate": 1.05
+          },
+      });
+      const eofr = new MessageEmbed()
+      .setAuthor(`| Nightcore is Enabled`, message.author.displayAvatarURL({ dynamic: true }))
+       .setFooter(`Tip : Use ${prefix}clearfilters to clear`)
+      .setColor(ee.color)
+      return message.reply({embeds: [eofr], allowedMentions: { repliedUser: false }});
+    } catch (e) {
+      console.log(String(e.stack).bgRed)
+      const emesdf = new MessageEmbed()
+      .setColor(ee.wrongcolor)
+      .setAuthor(`An Error Occurred`)
+      .setDescription(`\`\`\`${e.message}\`\`\``);
+      return message.channel.send({embeds: [emesdf]})
+    }
+  }
+};
