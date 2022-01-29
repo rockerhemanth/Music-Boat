40d00a5c63dea438d78b57ce278d09d9fddc57b0
M S V <78690237+Just-Msv@users.noreply.github.com>
2022-01-29 15:07:05 +0530

    Fully Updates Version

---
+++ commands/Filter/clearfilters.js
@@ -0,0 +1,52 @@
+const {
+  MessageEmbed
+} = require(`discord.js`);
+const DBL = require('@top-gg/sdk');
+const config = require(`../../botconfig/config.json`);
+const ee = require(`../../botconfig/embed.json`);
+const emoji = require(`../../botconfig/emojis.json`);
+module.exports = {
+  name: `reset`,
+  category: `Filter`,
+  aliases: ["cf", "clearfilter","clear filters", "clearfilters"],
+  description: `Clears the Equalizer`,
+  usage: `clearfilter`,
+  parameters: {"type":"music", "activeplayer": true, "previoussong": false},
+  run: async (client, message, args, guildData, player, prefix) => {
+
+    try {
+
+        
+     
+           
+            
+      player.clearEQ();
+      player.node.send({
+        op: "filters",
+        guildId: message.guild.id,
+        equalizer: player.bands.map((gain, index) => {
+            var Obj = {
+              "band": 0,
+              "gain": 0,
+            };
+            Obj.band = Number(index);
+            Obj.gain = Number(gain)
+            return Obj;
+          }),
+      });
+      const isdfr = new MessageEmbed()
+      .setColor(ee.color)
+      .setAuthor(`| Successfully Cleared All The Filter.`, message.author.displayAvatarURL( { dynamic: true } ));
+    
+      return message.reply({embeds: [isdfr], allowedMentions: { repliedUser: false }});
+            
+    } catch (e) {
+      console.log(String(e.stack).bgRed)
+      const emesdf = new MessageEmbed()
+      .setColor(ee.wrongcolor)
+      .setAuthor(`An Error Occurred`)
+      .setDescription(`\`\`\`${e.message}\`\`\``);
+      return message.channel.send({embeds: [emesdf]})
+    }
+  }
+};
