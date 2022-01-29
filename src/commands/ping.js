d6422789bdeb1219d176871804e664d62a816789
M S V <78690237+Just-Msv@users.noreply.github.com>
2022-01-29 14:54:48 +0530

    Delete src directory

--- src/commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "ping",
-    aliases: ["pi"],
-    exec: (msg,text) => {
-        const latency = Date.now() - msg.createdTimestamp;
-        msg.channel.send(`🏓 Pong! \`${latency}ms\`. API Latency is \`${Math.round(msg.client.ws.ping)}ms\``);
-    }
-};
-
--- src/commands/source.js
+++
@@ -1,9 +0,0 @@
-const util = require("../util");
-
-module.exports = {
-    name: "source",
-    aliases: ["src"],
-    exec: (msg) => {
-        msg.channel.send(util.embed().setDescription("✅ | [Here](https://github.com/Just-Msv/Msv-Music) is the open source repository this bot uses."));
-    }
-};
