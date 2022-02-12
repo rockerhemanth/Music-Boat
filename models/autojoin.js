57dba8a8c7e7f9fd9670f90dd749a7bb71b6ccb1
M S V <78690237+Just-Msv@users.noreply.github.com>
2022-02-12 17:53:33 +0530

    Delete autojoin.js

--- models/autojoin.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const autojoinSchema = new Schema({
-    guildID: String,
-    voiceChannel: String,
-    textChannel: String
-})
-
-module.exports = model('autojoin', autojoinSchema);
