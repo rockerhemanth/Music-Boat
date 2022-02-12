bcd1fada2ff015ffc1f834b1702189a3f9e61af0
M S V <78690237+Just-Msv@users.noreply.github.com>
2022-02-12 17:53:40 +0530

    Delete redeemCode.js

--- models/redeemCode.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const PremiumSchema = new Schema({
-  Usage: Number,
-  Code: String,
-  Expiry: Number
-});
-
-module.exports = model("redeemCodes", PremiumSchema);
